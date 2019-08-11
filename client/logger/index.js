08f845ac0c5873c231a79545d3b8b93bbf542160
Ashish Bachavala <ashish0810@yahoo.com>
2019-08-11 02:45:03 -0700

    Fix logger

--- client/logger/index.js
+++ client/logger/index.js
@@ -1,17 +1,13 @@
-export const debug = (message) => {
-    console.log(message);
-}
+export default class Logger {
+    static debug(message) {
+        console.log(message);
+    }
 
-export const info = (message) => {
-    console.log(message);
-}
+    static info(message) {
+        console.log(message);
+    }
 
-export const warn = (message) => {
-    console.log(message);
-}
-
-export default Logger = {
-    debug: debug,
-    info: info,
-    warn: warn,
+    static warn(message) {
+        console.log(message);
+    }
 }
