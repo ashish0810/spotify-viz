9ff2cb5e8da305a622200e644b1da490e42de9cc
Ashish Bachavala <ashish0810@yahoo.com>
2019-08-12 00:36:16 -0700

    Adding toggle fullscreen functionality

--- client/index.js
+++ client/index.js
@@ -27,4 +27,31 @@ function play() {
 }
 window.play = play;
 
+function fullscreen() {
+  const elem = document.documentElement;
+  if(window.innerWidth == screen.width && window.innerHeight == screen.height) {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+      document.msExitFullscreen();
+    }
+  } else {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
+  }
+}
+window.toggleFull = fullscreen;
+
+
 window.Version = VERSION;
